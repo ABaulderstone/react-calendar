59d44d0eecc7cb4560515e6d6174599f1d8aefca
Abaulderstone <alex.baulderstone@coderacademy.edu.au>
2023-06-06 10:47:34 +1000

    updates heights

--- src/components/Calendar/styled/index.tsx
+++ src/components/Calendar/styled/index.tsx
@@ -1,7 +1,7 @@
 import tw from 'twin.macro';
 import Table from '../../Table';
 
-export const Container = tw.div`container mx-auto px-4 flex flex-col gap-4`;
+export const Container = tw.div`container mt-2 mx-auto px-4 flex flex-col gap-8`;
 export const CalendarTable = ({ children, ...props }: any) => {
   return (
     <Table {...props} tw='w-full table-fixed'>
@@ -14,7 +14,7 @@ export const CalendarCell = ({ currentMonth, date }: any) => {
   return (
     <td
       css={[
-        tw`border border-gray-700 p-3 w-[calc(100%/7)] sm:h-16 md:h-24 lg:h-32 text-left align-top`,
+        tw`border border-gray-700 p-3 w-[calc(100%/7)] h-24 md:h-32 lg:h-48 text-left align-top`,
         !currentMonth && tw`bg-slate-200`,
       ]}
     >
